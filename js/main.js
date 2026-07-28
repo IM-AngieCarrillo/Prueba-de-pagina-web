604d22421cb71e2c81e0208d8bb36edf9a5d663f
Angie Carrillo <ancarrilloc@gmail.com>
2026-07-28 05:02:42 +0000

    Avance 13: Creacion de codigo en JSON y Java primera versión preliminar. Cambios en HTML y CSS para agregar información.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
-function showTime() {
-	document.getElementById('currentTime').innerHTML = new Date().toUTCString();
-}
-showTime();
-setInterval(function () {
-	showTime();
-}, 1000);
+//Seleccionamos los botones que necesitamos con el atributo data-language
+const langButtons = document.querySelectorAll("[data-language]");
+//la constante textos a cambiar cambiara los que tengan un data-section
+const textsToChange = document.querySelectorAll("[data-section]");
+//hacemos un for each por cada boton que listen y llame funcion
+langButtons.forEach((button) => {
+//confirmar que si funcione escuchando en console
+	//console.log(button.dataset.language);
+	fetch('../languages/${button.dataset.language}.json')
+	.then(res=> res.json())
+	//.then(data => console.log(data))
+	.then(data => {
+		textsToChange.forEach((el) => {
+			const section = el.dataset.section;
+			const value = el.dataset.value;
+			el.innerHTML = data[section][value];
+			
+		})
+	})
+})
