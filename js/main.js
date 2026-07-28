b86523bc969c5074fc058c3dca55b38d27c02198
Angie Carrillo <ancarrilloc@gmail.com>
2026-07-28 05:20:27 +0000

    Avance 17: Cambio en codigo JSON por error creacion de constante en lugar de partir de boton directamente

--- js/main.js
+++ js/main.js
@@ -1,11 +1,32 @@
+/* old code borrar despues
+//Seleccionamos los botones que necesitamos con el atributo data-language
+const langButtons = document.querySelectorAll("[data-language]");
+
+//la constante textos a cambiar cambiara los que tengan un data-section
+
+const textsToChange = document.querySelectorAll("[data-section]");
+//hacemos un for each por cada boton que listen y llame funcion
+
+langButtons.forEach((button) => {
+//confirmar que si funcione escuchando en console
+	fetch(`../languages/${button.dataset.language}.json`)
+	.then(res=> res.json())
+	.then(data => {
+		textsToChange.forEach((el) => {
+			const section = el.dataset.section;
+			const value = el.dataset.value;
+			el.innerHTML = data[section][value];
+
+		})
+	})
+})*/
 // Seleccionamos los botones de idioma y los elementos a traducir
 const langButtons = document.querySelectorAll("[data-language]");
 const textsToChange = document.querySelectorAll("[data-section]");
 
-// Función para cambiar el idioma según el código ("es", "en", etc.)
+// Función para cambiar el idioma
 const changeLanguage = async (language) => {
     try {
-        // Usamos ruta relativa desde la raíz del proyecto
         const requestJson = await fetch(`./languages/${language}.json`);
         const data = await requestJson.json();
 
